d037fa1038f6b0cfef732ee5f548fda957677c03
RAJ <raj.te027@yahoo.com>
2021-03-23 16:21:56 +0530

    GetOracleData

--- getOracleData.py
+++ getOracleData.py
@@ -6,3 +6,9 @@ spark = pyspark.sql.SparkSession.builder.master("local[*]").config("spark.jar","
 url ="jdbc:oracle:thin://localhost:1521/orcl"
 df = spark.read.format("jdbc").option("url",url).option("user","scott").option("password","tiger").option("driver","oracle.jdbc.OracleDriver").option("dbtable","DEPT").load()
 df.show()
+
+#jars="F:\bigdata\Softwares\drivers"
+#spark = pyspark.sql.SparkSession.builder.master("local[*]").config("spark.driver.extraClassPath",jars).config("spark.executor.extraClassPath",jars).appName("csvdata").getOrCreate()
+#url = "jdbc:mysql://mysqldb.cmpuk2t3n5oa.ap-south-1.rds.amazonaws.com:3306/mysqldb"
+#df = spark.read.format("jdbc").option("url",url).option("user","musername").option("password","mpassword").option("driver","com.mysql.jdbc.Driver").option("dbtable","my10krecord").load()
+#df.show()
